bfb4f8c295b95c0d768e3195b1cb132810615b5f
James Kelson <kelsomfire@hotmail.com>
2019-12-29 21:06:02 -0300

    Finalizado apenas com Requests

--- DownloadManga.py
+++ DownloadManga.py
@@ -2,25 +2,17 @@ import requests
 import os
 from bs4 import BeautifulSoup
 import re
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from tqdm import tqdm
+import requests_cache
+
+requests_cache.install_cache('DownloadManga_cache')
 
 class downloadManga:
     def setUp(self, manga):
         self.url_base = 'http://centraldemangas.online'
-        self.displayMsg('Manga', manga)
-        manga = manga.replace(' ', '-')
+        self.displayMsg('Manga', manga.lower().capitalize())
+        manga = manga.replace(' ', '-').lower()
         self.searchManga(manga)
-
-   
-
-    def downloadImg(self,url,manga):
-        response = requests.get(url)
-        if response.status_code == 200:
-            with open(manga +"/capa.jpg", 'wb') as f:
-                f.write(response.content)
-                print('Download da capa!!!')
 
     def createDirectory(self):
         try:
@@ -40,8 +32,8 @@ class downloadManga:
                 print(
                     'Esta página foi removida, não existe mais, ou talvez nunca tenha existido!')
             else:
-                self.descriptionManga()
                 self.createDirectory()
+                self.descriptionManga()
                 self.loadLinks()
 
         else:
@@ -75,15 +67,49 @@ class downloadManga:
         self.displayMsg('Autor', autor)
         self.displayMsg('Gênero', genero)
         self.displayMsg('Status', status)
-        self.downloadImg(capa,self.manga)
+        self.downloadImg(capa,self.manga,'')
 
     def loadLinks(self):
-        bs = self.bs
-        self.driver = webdriver.Chrome()
         links=[]
-        [links.append(link['href']) for link in bs.find_all(href=re.compile('/titulos/'))]
+        [links.append(link['href']) for link in self.bs.find_all(href=re.compile('/titulos/'))]
         for i in tqdm(range(len(links))):
-            self.driver.get(self.url_base + links[i])
+            url = self.url_base + links[i]
+            response = requests.get(url)
+            bs  = BeautifulSoup(response.text,'html.parser')
+            itens = len(bs.find_all('script'))-1
+            i = len((bs.find_all('script')[itens].get_text()).split())-1
+            parm = (bs.find_all('script')[itens].get_text()).split()
+            urlSulfix = parm[15].replace("'","").replace(';','')
+            pages = parm[i-1]
+
+            for char in "'[];":
+                pages = pages.replace(char,'')
+
+            for page in pages[:-1].split(','):
+                urlImg = urlSulfix + page +'.jpg'
+                self.downloadImg(url,self.manga,urlImg)
+    
+    def downloadImg(self,url,manga,urldownload):
+        if len(urldownload)==0:
+            response = requests.get(url)
+            if response.status_code == 200:
+                with open(manga +"/capa.jpg", 'wb') as f:
+                    f.write(response.content)
+                    print('Download da capa!!!')
+        else:
+            headers = {
+                        'Connection': 'keep-alive',
+                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36 OPR/65.0.3467.78',
+                        'Accept': 'image/webp,image/apng,image/*,*/*;q=0.8',
+                        'Referer': url,
+                        'Accept-Encoding': 'gzip, deflate',
+                        'Accept-Language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
+                        }
+            response = requests.get(urldownload, headers=headers, verify=False)
+            mangaSplit = urldownload.split('/')
+            mangaImg = mangaSplit[len(mangaSplit)-1]
+            with open(manga +"/" + mangaImg, 'wb') as f:
+                    f.write(response.content)
 
 manga = str(input('Digite o nome do manga: '))
 d = downloadManga()
